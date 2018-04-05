81ef0ce4badac89b209717072d0f08bb703b54fc
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2018-04-05 14:13:34 +1000

    loading multiple items in flatlist

--- react-client/skyjacs_client/src/components/home.js
+++ react-client/skyjacs_client/src/components/home.js
@@ -1,58 +1,129 @@
 import React from 'react';
-import { FlatList, Text, View, Image, ScrollView, ActivityIndicator } from 'react-native';
+import { FlatList, Text, View, Image, ScrollView, ActivityIndicator, StyleSheet } from 'react-native';
 import { List, ListItem, Avatar } from 'react-native-elements';
 import { TabNavigation } from 'react-navigation';
 
 export default class HomeScreen extends React.Component {
-constructor(props){
-    super(props);
-    this.state ={ isLoading: true,
-    base_url: "http://django-env.unwf22fga6.ap-southeast-2.elasticbeanstalk.com"}
-  }
+	constructor(props){
+		super(props);
+		this.state ={ isLoading: true,
+		base_url: "http://django-env.unwf22fga6.ap-southeast-2.elasticbeanstalk.com"}
+	}	
 
-  componentDidMount(){
-    return fetch('http://django-env.unwf22fga6.ap-southeast-2.elasticbeanstalk.com/shoes/?format=json')
-      .then((response) => response.json())
-      .then((responseJson) => {
+  	componentDidMount(){
+    	return fetch('http://django-env.unwf22fga6.ap-southeast-2.elasticbeanstalk.com/shoes/?format=json')
+      	.then((response) => response.json())
+      	.then((responseJson) => {
 
         this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
+			isLoading: false,
+          	dataSource: responseJson,
+        },function(){
 
         });
 
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
+  		})
+      		.catch((error) =>{
+        	console.error(error);
+  		});
+	}
+
+	renderSeperator = () => {
+		return(
+			<View style={styles.seperator}/>
+		);
+	};
 
 
+	render(){
+	    if(this.state.isLoading){
+			return(
+	        <View style={{flex: 1, padding: 20}}>
+				<ActivityIndicator/>
+	        </View>
+			)
+	    }
+	    return(
+			<ScrollView>
 
-  render(){
+			<View style={styles.header}>
+			<Text style={{backgroundColor: 'green'}}>Header</Text>
+			</View>
 
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
+			<View style={styles.latest}>
+				<Text style={{backgroundColor: 'green'}}>Latest</Text>
+		        <FlatList
+		        	horizontal={true}
+					data={this.state.dataSource}
+		          	renderItem={({item}) => <Image 
+						source={{uri: `${item.photo}`}} 
+						style={{height: 100, width: 150}}
+						resizeMode= 'cover'
+		        		/>
+		    		}		
+		    		ItemSeperatorComponent={this.renderSeperator}
+		          	keyExtractor={(item, id) => id}
+		        />
+			</View>
 
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Image 
-				source={{uri: `${item.photo}`}} 
-				style={{height: 600, width: 600}}
-				resizeMode= 'cover'
-        		/>
-    		}		
-          keyExtractor={(item, id) => id}
-        />
-      </View>
-    );
-  }
+			<View style={styles.popular}>
+				<Text style={{backgroundColor: 'green'}}>Popular</Text>
+				<FlatList
+		        	horizontal={true}
+					data={this.state.dataSource}
+		          	renderItem={({item}) => <Image 
+						source={{uri: `${item.photo}`}} 
+						style={{height: 100, width: 150}}
+						resizeMode= 'cover'
+		        		/>
+		    		}		
+		          	keyExtractor={(item, id) => id}
+		        />
+			</View>
+
+			<View style={styles.recent}>
+				<Text style={{backgroundColor: 'green'}}>Recently Viewed</Text>
+				<FlatList
+		        	horizontal={true}
+					data={this.state.dataSource}
+		          	renderItem={({item}) => <Image 
+						source={{uri: `${item.photo}`}} 
+						style={{height: 100, width: 150}}
+						resizeMode= 'cover'
+		        		/>
+		    		}		
+		          	keyExtractor={(item, id) => id}
+		        />
+			</View>
+
+
+	  		</ScrollView>
+	    );
+
+  	}
 }
+
+const styles = StyleSheet.create({
+	header: {
+		height: 260, 
+		backgroundColor: '#6eb7ac',
+	},
+	latest: {
+		height: 200, 
+		backgroundColor: '#fbefda',
+	},
+	popular: {
+		height: 200, 
+		backgroundColor: '#aaaaaa',
+	},
+	recent: {
+		height: 200,
+		backgroundColor: '#b76e79',
+	},
+	seperator: {
+		paddingLeft: "10%",
+	},
+});
+
+
+
