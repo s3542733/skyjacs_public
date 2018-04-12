eefa7d0ad7b8b1e418207d62f5d012e7f82e577f
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2018-04-12 13:07:28 +1000

    more server modifications

--- django-server/skyjacs_server/skyjacs_app/serializers.py
+++ django-server/skyjacs_server/skyjacs_app/serializers.py
@@ -1,13 +1,34 @@
-from django.contrib.auth.models import User, Group
+#from django.contrib.auth.models import User, Group
+from skyjacs_app.models import User, Listing, Notification, Image
 from rest_framework import serializers
-from .models import Shoe
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = User
-		fields = ('id', 'url', 'username', 'email')
+		fields = ('uid', 'full_name', 'email_address', 'user_admin')
 
-class ShoeSerializer(serializers.ModelSerializer):
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
-		model = Shoe
-		fields = ('id', 'date_added', 'title', 'photo')
+		model = Listing
+		fields = ('uid', 'user', 'listing_type', 'listing_title', 'listing_date',
+			'item_sex', 'sex_priority', 'sex_strict', 'item_type', 'type_priority', 'type_strict', 
+			'item_brand', 'brand_priority', 'brand_strict', 'item_model', 'model_priority', 
+			'model_strict', 'item_condition', 'condition_priority', 'condition_strict', 
+			'item_colour', 'item_priority', 'item_strict', 'item_material', 'material_priority', 
+			'material_strict', 'item_size', 'size_priority', 'size_strict', 'item_notes', 'item_matching')
+
+class NotificationSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Notification
+		fields = ('uid', 'user', 'user_listing_id', 'matched_listing_id')
+
+#class SpecSerializer(serializers.HyperlinkedModelSerializer):
+#	class Meta:
+#		model = Spec
+#		fields = ('uid', 'listing', 'item_type', 'item_brand', 'item_model', 'item_condition', 'item_colour', 'item_material', 
+#			'item_material' ,'item_size', 'item_notes')
+
+class ImageSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Image
+		fields = ('uid', 'listing', 'image_url')
