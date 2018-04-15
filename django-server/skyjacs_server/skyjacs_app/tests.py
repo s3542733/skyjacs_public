2163aef8b3abac5ae50dcddd948ab179befa2e4d
Darkside-PC <s3506883@student.rmit.edu.au>
2018-04-15 20:47:21 +0900

    add test case for views

--- django-server/skyjacs_server/skyjacs_app/tests.py
+++ django-server/skyjacs_server/skyjacs_app/tests.py
@@ -1,22 +1,33 @@
 from django.test import TestCase
 from .models import User
+from .views import *
 
-class ModelTestCase(TestCase):
-    #
+class SkyjacsTestCase(TestCase):
+    #testing user model
 
-    def setUp(self):
-        #
-        self.User_name = "test"
-        self.email = "test@email.com"
-        self.user_admin = 'False'
-        self.user = User(full_name=self.User_name,email_address=self.email, user_admin=self.user_admin)
+    def create_user(self, User_name='test', email='test@email.com',user_admin='False'):
+        #setup testing user object
+        return User.objects.create(full_name=User_name,email_address=email, user_admin=user_admin)
 
     def test_model_create_user(self):
-        #
+        #check if user object is created
         old_count = User.objects.count()
+        self.user = self.create_user()
         self.user.save()
         new_count = User.objects.count()
         self.assertNotEqual(old_count, new_count)
 
     def test_model_check_created_user(self):
-        self.assertEqual(self.User_name, self.email, self.user_admin)
+        #
+        newUser = self.create_user()
+        self.assertTrue(isinstance(newUser, User))
+        self.assertEqual('test@email.com', newUser.email_address)
+        self.assertEqual('test',newUser.full_name)
+        self.assertEqual('False',newUser.user_admin)
+
+    def test_skyjacs_matchType(self):
+        #testing users view
+        empty_pkSpec = matchType('','something','type')
+        self.assertEqual(empty_pkSpec, -1)
+        empty_dbSpec = matchType('something','','type')
+        self.assertEqual(empty_dbSpec, 100)
